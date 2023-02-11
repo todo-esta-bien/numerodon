92b3f29087d3850db4e8e633b7eb1794e9e6a26c
Rodrigo Medina <rodrigom@lyft.com>
2023-02-10 20:07:54 -0600

    Fixed paths

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,9 @@
 import typescript from "@rollup/plugin-typescript";
-// import tsConfigPaths from "rollup-plugin-ts-paths";
 
 export default {
   input: "src/index.ts",
   output: {
     dir: "dist",
-    format: "cjs",
   },
-  plugins: [
-    typescript(),
-    // tsConfigPaths(),
-  ],
+  plugins: [typescript()],
 };
