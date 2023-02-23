ce1bdafe0ee8b9d4d5309e12d348888eda5b6716
Rodrigo Medina <rodrigom@lyft.com>
2023-02-23 00:19:24 -0600

    Ended pinnacles calculations

--- src/pythagorean/pythagorean.test.ts
+++ src/pythagorean/pythagorean.test.ts
@@ -1,6 +1,11 @@
-import { PythagoreanProfile, IPythagoreanProfileConstructor } from "./pythagorean";
+import {
+  PythagoreanProfile,
+  IPythagoreanProfileConstructor,
+  PythagoreanPinnacle,
+  IPythagoreanPinnacleConstructor,
+} from "./pythagorean";
 
-type TestCase = {
+type PythagoreanProfileTestCase = {
   original: IPythagoreanProfileConstructor;
   expected: Omit<
     PythagoreanProfile,
@@ -17,7 +22,7 @@ type TestCase = {
   >;
 };
 
-const profileTestCases: TestCase[] = [
+const pythagoreanProfileTestCases: PythagoreanProfileTestCase[] = [
   {
     original: {
       day: 23,
@@ -41,7 +46,7 @@ const profileTestCases: TestCase[] = [
 ];
 
 describe("Testing PythagoreanProfile", () => {
-  it.each(profileTestCases)("PythagoreanProfile($original)", ({ original, expected }) => {
+  it.each(pythagoreanProfileTestCases)("PythagoreanProfile($original)", ({ original, expected }) => {
     // Act
     const profile: PythagoreanProfile = new PythagoreanProfile(original);
 
@@ -81,5 +86,319 @@ describe("Testing PythagoreanProfile", () => {
 
     // Assert
     expect(result).toBe(4);
+  });
+});
+
+type PythagoreanPinnacleTestCase = {
+  original: IPythagoreanPinnacleConstructor;
+  expected: Omit<PythagoreanPinnacle, "pythagoreanPinnacleSumOptions" | "day" | "month" | "year">;
+};
+
+const pythagoreanPinnacleTestCases: PythagoreanPinnacleTestCase[] = [
+  {
+    original: {
+      day: 13,
+      month: 3,
+      year: 1963,
+    },
+    expected: {
+      karma: 3, // A
+      personal: 4, // B
+      pastLife: 1, // C
+      personality: 8, // D
+
+      firstRealization: 7, // E
+      secondRealization: 5, // F
+      thirdRealization: 3, // G
+      fourthRealization: 22, // H
+      destiny: 22, // H
+
+      subconscious: 6, // I
+      unconscious: 3, // J
+
+      firstGoal: 1, // K
+      secondGoal: 3, // L
+      thirdGoal: 2, // M
+      fourthGoal: 2, // N
+
+      negativeUnconscious: 6, // O
+      shadow: 5, // P
+
+      familyInferiorBeing: 3, // Q
+      consciousInferiorBeing: 5, // R
+      latentInferiorBeing: 8, // S
+
+      superHiddens: [], // W
+    },
+  },
+  {
+    original: {
+      day: 16,
+      month: 8,
+      year: 1980,
+    },
+    expected: {
+      karma: 8, // A
+      personal: 7, // B
+      pastLife: 9, // C
+      personality: 6, // D
+
+      firstRealization: 6, // E
+      secondRealization: 7, // F
+      thirdRealization: 4, // G
+      fourthRealization: 8, // H
+      destiny: 8, // H
+
+      subconscious: 8, // I
+      unconscious: 5, // J
+
+      firstGoal: 1, // K
+      secondGoal: 2, // L
+      thirdGoal: 1, // M
+      fourthGoal: 1, // N
+
+      negativeUnconscious: 4, // O
+      shadow: 1, // P
+
+      familyInferiorBeing: 2, // Q
+      consciousInferiorBeing: 3, // R
+      latentInferiorBeing: 5, // S
+
+      superHiddens: [3], // W
+    },
+  },
+  {
+    original: {
+      day: 27,
+      month: 12,
+      year: 1994,
+    },
+    expected: {
+      karma: 3, // A
+      personal: 9, // B
+      pastLife: 5, // C
+      personality: 8, // D
+
+      firstRealization: 3, // E
+      secondRealization: 5, // F
+      thirdRealization: 8, // G
+      fourthRealization: 8, // H
+      destiny: 8, // H
+
+      subconscious: 7, // I
+      unconscious: 7, // J
+
+      firstGoal: 6, // K
+      secondGoal: 4, // L
+      thirdGoal: 2, // M
+      fourthGoal: 2, // N
+
+      negativeUnconscious: 3, // O
+      shadow: 11, // P
+
+      familyInferiorBeing: 8, // Q
+      consciousInferiorBeing: 6, // R
+      latentInferiorBeing: 5, // S
+
+      superHiddens: [], // W
+    },
+  },
+  {
+    original: {
+      day: 24,
+      month: 2,
+      year: 1960,
+    },
+    expected: {
+      karma: 2, // A
+      personal: 6, // B
+      pastLife: 7, // C
+      personality: 6, // D
+
+      firstRealization: 8, // E
+      secondRealization: 22, // F
+      thirdRealization: 3, // G
+      fourthRealization: 9, // H
+      destiny: 9, // H
+
+      subconscious: 33, // I
+      unconscious: 6, // J
+
+      firstGoal: 4, // K
+      secondGoal: 1, // L
+      thirdGoal: 3, // M
+      fourthGoal: 5, // N
+
+      negativeUnconscious: 8, // O
+      shadow: 5, // P
+
+      familyInferiorBeing: 7, // Q
+      consciousInferiorBeing: 4, // R
+      latentInferiorBeing: 11, // S
+
+      superHiddens: [], // W
+    },
+  },
+  {
+    original: {
+      day: 5,
+      month: 12,
+      year: 1962,
+    },
+    expected: {
+      karma: 3, // A
+      personal: 5, // B
+      pastLife: 9, // C
+      personality: 8, // D
+
+      firstRealization: 8, // E
+      secondRealization: 5, // F
+      thirdRealization: 22, // G
+      fourthRealization: 3, // H
+      destiny: 3, // H
+
+      subconscious: 8, // I
+      unconscious: 11, // J
+
+      firstGoal: 2, // K
+      secondGoal: 4, // L
+      thirdGoal: 2, // M
+      fourthGoal: 6, // N
+
+      negativeUnconscious: 8, // O
+      shadow: 7, // P
+
+      familyInferiorBeing: 4, // Q
+      consciousInferiorBeing: 6, // R
+      latentInferiorBeing: 1, // S
+
+      superHiddens: [], // W
+    },
+  },
+  {
+    original: {
+      day: 6,
+      month: 7,
+      year: 1935,
+    },
+    expected: {
+      karma: 7, // A
+      personal: 6, // B
+      pastLife: 9, // C
+      personality: 22, // D
+
+      firstRealization: 4, // E
+      secondRealization: 6, // F
+      thirdRealization: 1, // G
+      fourthRealization: 7, // H
+      destiny: 7, // H
+
+      subconscious: 11, // I
+      unconscious: 11, // J
+
+      firstGoal: 1, // K
+      secondGoal: 3, // L
+      thirdGoal: 2, // M
+      fourthGoal: 2, // N
+
+      negativeUnconscious: 6, // O
+      shadow: 1, // P
+
+      familyInferiorBeing: 3, // Q
+      consciousInferiorBeing: 5, // R
+      latentInferiorBeing: 8, // S
+
+      superHiddens: [], // W
+    },
+  },
+  {
+    original: {
+      day: 7,
+      month: 11,
+      year: 1995,
+    },
+    expected: {
+      karma: 11, // A
+      personal: 7, // B
+      pastLife: 6, // C
+      personality: 6, // D
+
+      firstRealization: 9, // E
+      secondRealization: 4, // F
+      thirdRealization: 4, // G
+      fourthRealization: 8, // H
+      destiny: 8, // H
+
+      subconscious: 8, // I
+      unconscious: 5, // J
+
+      firstGoal: 5, // K
+      secondGoal: 1, // L
+      thirdGoal: 4, // M
+      fourthGoal: 4, // N
+
+      negativeUnconscious: 1, // O
+      shadow: 7, // P
+
+      familyInferiorBeing: 9, // Q
+      consciousInferiorBeing: 5, // R
+      latentInferiorBeing: 5, // S
+
+      superHiddens: [6], // W
+    },
+  },
+];
+
+describe("Testing PythagoreanPinnacle", () => {
+  it.each(pythagoreanPinnacleTestCases)("PythagoreanPinnacle($original)", ({ original, expected }) => {
+    // Act
+    const profile: PythagoreanPinnacle = new PythagoreanPinnacle(original);
+
+    // Assert
+    expect(profile.karma).toBe(expected.karma);
+    expect(profile.personal).toBe(expected.personal);
+    expect(profile.pastLife).toBe(expected.pastLife);
+    expect(profile.personality).toBe(expected.personality);
+
+    expect(profile.firstRealization).toBe(expected.firstRealization);
+    expect(profile.secondRealization).toBe(expected.secondRealization);
+    expect(profile.thirdRealization).toBe(expected.thirdRealization);
+    expect(profile.fourthRealization).toBe(expected.fourthRealization);
+    expect(profile.destiny).toBe(expected.destiny);
+
+    expect(profile.subconscious).toBe(expected.subconscious);
+    expect(profile.unconscious).toBe(expected.unconscious);
+
+    expect(profile.firstGoal).toBe(expected.firstGoal);
+    expect(profile.secondGoal).toBe(expected.secondGoal);
+    expect(profile.thirdGoal).toBe(expected.thirdGoal);
+    expect(profile.fourthGoal).toBe(expected.fourthGoal);
+
+    expect(profile.negativeUnconscious).toBe(expected.negativeUnconscious);
+    expect(profile.shadow).toBe(expected.shadow);
+
+    expect(profile.familyInferiorBeing).toBe(expected.familyInferiorBeing);
+    expect(profile.consciousInferiorBeing).toBe(expected.consciousInferiorBeing);
+    expect(profile.latentInferiorBeing).toBe(expected.latentInferiorBeing);
+
+    expect(profile.superHiddens).toMatchObject(expected.superHiddens);
+  });
+
+  it.each([
+    [
+      [3, 15, 3, 12, 15, 12, 4, 15, 5, 12, 2, 15],
+      [9, 9],
+    ],
+    [[3, 15, 3, 12, 15, 12, 4, 15, 5], [9]],
+    [[3, 15, 3, 12, 15, 12, 4, 5], []],
+  ])("%s should generate %s super hiddens", (negativeNumbers: number[], expected: number[]) => {
+    // Arrange
+    const pinnacle = new PythagoreanPinnacle({ day: 0, month: 0, year: 0 });
+
+    // Act
+    const result = pinnacle["calculateSuperHidden"](negativeNumbers);
+
+    // Assert
+    expect(result).toMatchObject(expected);
   });
 });
--- src/pythagorean/pythagorean.ts
+++ src/pythagorean/pythagorean.ts
@@ -43,7 +43,7 @@ export class PythagoreanProfile {
     this.fatherLastNames = fatherLastNames;
     this.motherLastNames = motherLastNames;
 
-    this.completeName = `${this.names} ${this.fatherLastNames} ${this.motherLastNames}`;
+    this.completeName = `${names} ${fatherLastNames} ${motherLastNames}`;
     this.completeNameSumResult = getLetterSumFromString(this.completeName, this.pythagoreanSumOptions);
 
     this.soul = this.completeNameSumResult.vowelSum;
@@ -60,11 +60,11 @@ export class PythagoreanProfile {
     );
 
     this.balance = this.getCompleteNameInitialsSum(this.completeName);
-    this.strength = reduceNumberDigits(this.day + this.month, this.pythagoreanSumOptions);
-    this.lifePath = reduceNumberDigits(this.day + this.month + this.year, this.pythagoreanSumOptions);
+    this.strength = reduceNumberDigits(day + month, this.pythagoreanSumOptions);
+    this.lifePath = reduceNumberDigits(day + month + year, this.pythagoreanSumOptions);
 
     this.spiritualInitiation = reduceNumberDigits(
-      this.soul + this.expression + this.day + this.lifePath,
+      this.soul + this.expression + day + this.lifePath,
       this.pythagoreanSumOptions
     );
   }
@@ -76,5 +76,146 @@ export class PythagoreanProfile {
       .join("");
     const letterSumResult: LetterSumResult = getLetterSumFromString(concatenatedInitials, this.pythagoreanSumOptions);
     return letterSumResult.totalSum;
+  }
+}
+
+export interface IPythagoreanPinnacleConstructor {
+  day: number;
+  month: number;
+  year: number;
+}
+
+export class PythagoreanPinnacle {
+  public readonly month: number;
+  public readonly day: number;
+  public readonly year: number;
+
+  public readonly karma: number; // A
+  public readonly personal: number; // B
+  public readonly pastLife: number; // C
+  public readonly personality: number; // D
+
+  public readonly firstRealization: number; // E
+  public readonly secondRealization: number; // F
+  public readonly thirdRealization: number; // G
+  public readonly fourthRealization: number; // H
+  public readonly destiny: number; // H
+
+  public readonly subconscious: number; // I
+  public readonly unconscious: number; // J
+
+  public readonly firstGoal: number; // K
+  public readonly secondGoal: number; // L
+  public readonly thirdGoal: number; // M
+  public readonly fourthGoal: number; // N
+
+  public readonly negativeUnconscious: number; // O
+  public readonly shadow: number; // P
+
+  public readonly familyInferiorBeing: number; // Q
+  public readonly consciousInferiorBeing: number; // R
+  public readonly latentInferiorBeing: number; // S
+
+  public readonly superHiddens: number[]; // W
+
+  public readonly pythagoreanPinnacleSumOptions: ReduceNumberDigitsAttrs = {
+    sumRecursively: true,
+    stopNumbers: [11, 22, 33, 44],
+  };
+
+  constructor({ day, month, year }: IPythagoreanPinnacleConstructor) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+
+    this.karma = reduceNumberDigits(month, this.pythagoreanPinnacleSumOptions);
+    this.personal = reduceNumberDigits(day, this.pythagoreanPinnacleSumOptions);
+    this.pastLife = reduceNumberDigits(year, this.pythagoreanPinnacleSumOptions);
+    this.personality = reduceNumberDigits(day + month + year, this.pythagoreanPinnacleSumOptions);
+
+    this.firstRealization = reduceNumberDigits(month + day, this.pythagoreanPinnacleSumOptions);
+    this.secondRealization = reduceNumberDigits(day + year, this.pythagoreanPinnacleSumOptions);
+    this.thirdRealization = reduceNumberDigits(month + 2 * day + year, this.pythagoreanPinnacleSumOptions);
+    this.fourthRealization = reduceNumberDigits(month + year, this.pythagoreanPinnacleSumOptions);
+    this.destiny = this.fourthRealization;
+
+    // this.subconscious = reduceNumberDigits(2 * month + 4 * day + 2 * year, this.pythagoreanPinnacleSumOptions);
+    this.subconscious = reduceNumberDigits(
+      this.firstRealization + this.secondRealization + this.thirdRealization,
+      this.pythagoreanPinnacleSumOptions
+    );
+    // this.unconscious = reduceNumberDigits(2 * month + day + 2 * year, this.pythagoreanPinnacleSumOptions);
+    this.unconscious = reduceNumberDigits(
+      this.personality + this.fourthRealization,
+      this.pythagoreanPinnacleSumOptions
+    );
+
+    // Below numbers needs to use reduced versions (with no stop numbers) of day, month and year
+    const reducedMonth = reduceNumberDigits(month, { sumRecursively: true });
+    const reducedDay = reduceNumberDigits(day, { sumRecursively: true });
+    const reducedYear = reduceNumberDigits(year, { sumRecursively: true });
+
+    this.firstGoal = reduceNumberDigits(Math.abs(reducedMonth - reducedDay), this.pythagoreanPinnacleSumOptions);
+    this.secondGoal = reduceNumberDigits(Math.abs(reducedDay - reducedYear), this.pythagoreanPinnacleSumOptions);
+    this.thirdGoal = reduceNumberDigits(Math.abs(this.firstGoal - this.secondGoal), this.pythagoreanPinnacleSumOptions);
+    this.fourthGoal = reduceNumberDigits(Math.abs(reducedMonth - reducedYear), this.pythagoreanPinnacleSumOptions);
+
+    this.negativeUnconscious = reduceNumberDigits(
+      this.firstGoal + this.secondGoal + this.thirdGoal,
+      this.pythagoreanPinnacleSumOptions
+    );
+    this.shadow = reduceNumberDigits(this.personality + this.negativeUnconscious, this.pythagoreanPinnacleSumOptions);
+
+    this.familyInferiorBeing = reduceNumberDigits(this.firstGoal + this.thirdGoal, this.pythagoreanPinnacleSumOptions);
+    this.consciousInferiorBeing = reduceNumberDigits(
+      this.secondGoal + this.thirdGoal,
+      this.pythagoreanPinnacleSumOptions
+    );
+    this.latentInferiorBeing = reduceNumberDigits(
+      this.familyInferiorBeing + this.consciousInferiorBeing,
+      this.pythagoreanPinnacleSumOptions
+    );
+
+    this.superHiddens = this.calculateSuperHidden([
+      this.firstGoal,
+      this.secondGoal,
+      this.thirdGoal,
+      this.fourthGoal,
+      this.negativeUnconscious,
+      this.shadow,
+      this.familyInferiorBeing,
+      this.consciousInferiorBeing,
+      this.latentInferiorBeing,
+    ]);
+  }
+
+  private calculateSuperHidden(negativeNumbers: number[]): number[] {
+    const sortedNegativeNumbers = [...negativeNumbers].sort();
+
+    interface RepeatedNumbers {
+      [key: number]: number;
+    }
+
+    const repeatedNumbers: RepeatedNumbers = sortedNegativeNumbers.reduce(
+      (repeatedNumbers: RepeatedNumbers, currentNumber: number) => {
+        if (repeatedNumbers[currentNumber] === undefined) {
+          // Set initial value
+          repeatedNumbers[currentNumber] = 1;
+        } else {
+          // Increment number count
+          repeatedNumbers[currentNumber] += 1;
+        }
+
+        return repeatedNumbers;
+      },
+      {} as RepeatedNumbers
+    );
+
+    const bruteSuperHiddens = Object.entries(repeatedNumbers) // e.g. [['3', 1], ['12', 3], ['15', 4]]
+      .filter(([, count]: [string, number]) => count >= 3) // e.g. [['12', 3], ['15', 4]]
+      .map(([numberValue]: [string, number]) => +numberValue) // e.g. [12, 15]
+      .map((numberValue: number) => reduceNumberDigits(3 * numberValue, this.pythagoreanPinnacleSumOptions)); // e.g. [9, 9]
+
+    return bruteSuperHiddens;
   }
 }
