f2762ad3413a196f930a12fe0e4d667c54452706
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2023-05-24 21:41:19 -0600

    Added export statement

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export {
   PythagoreanPinnacle,
   IPythagoreanPinnacleConstructor,
 } from "./pythagorean/pythagorean";
+export { DestinyTable, IDestinyTableConstructor } from "./pythagorean/destinyTable";
 export { IEvolutiveProfileConstructor, EvolutiveNumbers, EvolutiveProfile } from "./evolutive/evolutive";
 export { IBase22ProfileConstructor, Base22Profile } from "./base22/base22";
