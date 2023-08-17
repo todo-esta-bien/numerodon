e379bf4d0ed1c862f2523b9e167f9df8c992264e
Rodrigo Medina <13385000+roeeyn@users.noreply.github.com>
2023-08-16 23:15:25 -0600

    feat: add missing exports (#19)
    * feat: add diamond profile
    
    * fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,4 @@ export {
 export { DestinyTable, IDestinyTableConstructor } from "./pythagorean/destinyTable";
 export { IEvolutiveProfileConstructor, EvolutiveNumbers, EvolutiveProfile } from "./evolutive/evolutive";
 export { IBase22ProfileConstructor, Base22Profile } from "./base22/base22";
+export { IDiamondProfileConstructor, DiamondProfile } from "./base22/diamond";
