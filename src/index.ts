ef43860416c53ed95cf0733a7b2308679037f273
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2023-11-13 10:27:18 -0600

    feat: export the new profile from index

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export { DestinyTable, IDestinyTableConstructor } from "./pythagorean/destinyTab
 export { IEvolutiveProfileConstructor, EvolutiveNumbers, EvolutiveProfile } from "./evolutive/evolutive";
 export { IBase22ProfileConstructor, Base22Profile } from "./base22/base22";
 export { IDiamondProfileConstructor, DiamondProfile } from "./base22/diamond";
+export { IPlannerProfileConstructor, PlannerProfile } from "./planner/planner";
